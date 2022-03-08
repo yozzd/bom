b90cbf621b978f1fe2d12a560b86708f1611e764
yozz <yozzdharma@gmail.com>
2022-03-08 13:27:23 +0700

    refactor: totalPricePerWO

--- server/schema/bom/resolvers.js
+++ server/schema/bom/resolvers.js
@@ -34,7 +34,7 @@ const resolvers = {
             'id', 'woNo', 'model', 'product', 'issued', 'unit', 'budget', 'difference',
             [sequelize.literal('COUNT(CASE WHEN `wos->headers->items`.bom_qty_balance >= 0 AND `wos->headers->items`.packing = 0 THEN 1 ELSE NULL END)'), 'totalIncoming'],
             [sequelize.literal('COUNT(CASE WHEN `wos->headers->items`.packing = 0 THEN 1 ELSE NULL END)'), 'totalItems'],
-            [sequelize.literal('SUM(`wos->headers->items`.bom_usd_total)'), 'totalPricePerWO'],
+            [sequelize.literal('SUM(CASE WHEN `wos->headers->items`.packing = 0 THEN `wos->headers->items`.bom_usd_total ELSE 0 END)'), 'totalPricePerWO'],
             [sequelize.literal('SUM(`wos->headers->items`.yet_to_purchase)'), 'totalYetToPurchase'],
             [sequelize.literal('SUM(CASE WHEN `wos->headers->items`.packing THEN `wos->headers->items`.bom_usd_total ELSE 0 END)'), 'totalPackingPerWO'],
           ],
