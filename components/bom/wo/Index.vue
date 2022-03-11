69387b413859db5b1864e27c935902a35fbfcadb
yozz <yozzdharma@gmail.com>
2022-03-11 15:08:44 +0700

    feat: add material specification column

--- components/bom/wo/Index.vue
+++ components/bom/wo/Index.vue
@@ -47,6 +47,13 @@
                 </p>
               </template>
             </el-table-column>
+            <el-table-column label="Specification" width="200" fixed>
+              <template slot-scope="scope">
+                <p :title="scope.row.bomSpecification" class="truncate">
+                  {{ scope.row.bomSpecification }}
+                </p>
+              </template>
+            </el-table-column>
           </el-table-column>
           <el-table-column label="" min-width="50"></el-table-column>
         </el-table>
