8bf81dd4c6bc630e8ed7e694b44e36320e1081ef
yozz <yozzdharma@gmail.com>
2022-03-14 14:48:45 +0700

    feat: outstanding-po route & empty page

---
+++ components/outstanding-po/Index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Outstanding PO
+  </div>
+</template>
---
+++ pages/outstanding-po/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <OutstandingPo />
+</template>
+
+<script>
+export default {
+  layout: 'layout1',
+  middleware: ['auth'],
+};
+</script>
