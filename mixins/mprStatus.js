267cec0751a4b6faaecaf13da62b3baa67eddd21
yozz <yozzdharma@gmail.com>
2022-03-17 11:57:27 +0700

    feat: add mprStatus mixin

---
+++ mixins/mprStatus.js
@@ -0,0 +1,22 @@
+export default {
+  data() {
+    return {
+      optionsMprStatus: [{
+        value: 0,
+        label: 'Open',
+      }, {
+        value: 1,
+        label: 'Process',
+      }, {
+        value: 4,
+        label: 'Hold',
+      }, {
+        value: 2,
+        label: 'Close',
+      }, {
+        value: 3,
+        label: 'Cancel',
+      }],
+    };
+  },
+};
