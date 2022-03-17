14b0b016c410bfd5edc49accb872300af7e39269
yozz <yozzdharma@gmail.com>
2022-03-17 12:02:02 +0700

    feat: add mprStatus to select option

--- components/mpr/Index.vue
+++ components/mpr/Index.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="flex flex-col space-y-8">
+    <div class="flex space-x-4 items-center">
+      <div class="w-48">
+        <el-select
+          v-model="status"
+          placeholder="Select"
+          class="el-select__primary"
+          @change="handleChange"
+        >
+          <el-option
+            v-for="item in optionsMprStatus"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </div>
+      <div class="flex-1"></div>
+    </div>
+
     <IndexErrorHandler
       v-if="errors"
       :errors="errors"
@@ -10,15 +30,22 @@
 </template>
 
 <script>
+import mprStatus from '../../mixins/mprStatus';
 import { GetAllMPR } from '../../apollo/mpr/mpr.query';
 
 export default {
+  mixins: [mprStatus],
   data() {
     return {
+      status: 0,
       items: {},
+      errors: [],
     };
   },
   methods: {
+    handleChange(v) {
+      this.status = v;
+    },
   },
   apollo: {
     getAllMPR: {
