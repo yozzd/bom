f0f0609649094e032dd5d43c330a25f905ea1dac
yozz <yozzdharma@gmail.com>
2022-03-17 09:20:10 +0700

    feat: add coming-row class highlighter

--- server/schema/bom/model.js
+++ server/schema/bom/model.js
@@ -405,7 +405,8 @@ const WOITEM = sequelize.define('item', {
     type: DataTypes.VIRTUAL,
     get() {
       if (this.validasi) return 'validated-row';
-      else if (this.bomQty > 0 && this.bomQtyStock > 0 && this.bomQtyBalance >= 0) return 'stocked-row';
+      else if (this.bomQty > 0 && this.bomQtyStock > 0 && this.bomQtyBalance >= 0) return 'stock-row';
+      else if (this.bomQtyRec > 0 && this.bomPoNo) return 'coming-row';
       return '';
     },
   },
