089dee1840474e79009a7c2c6427e07878d593d5
yozz <yozzdharma@gmail.com>
2022-03-17 11:37:00 +0700

    feat: register mpr to server schema

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,9 +1,11 @@
 const { typeDefs: Auth } = require('./auth/typeDefs');
 const { typeDefs: Bom } = require('./bom/typeDefs');
+const { typeDefs: Mpr } = require('./mpr/typeDefs');
 const { typeDefs: User } = require('./user/typeDefs');
 
 const { resolvers: authResolvers } = require('./auth/resolvers');
 const { resolvers: bomResolvers } = require('./bom/resolvers');
+const { resolvers: mprResolvers } = require('./mpr/resolvers');
 const { resolvers: userResolvers } = require('./user/resolvers');
 
 const Query = `
@@ -17,6 +19,6 @@ const Query = `
 `;
 
 module.exports = {
-  typeDefs: [Query, Auth, Bom, User],
-  resolvers: [authResolvers, bomResolvers, userResolvers],
+  typeDefs: [Query, Auth, Bom, Mpr, User],
+  resolvers: [authResolvers, bomResolvers, mprResolvers, userResolvers],
 };
