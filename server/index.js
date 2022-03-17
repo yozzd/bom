0afc0d93be469294ce3aa965e89935a900a9d15c
yozz <yozzdharma@gmail.com>
2022-03-17 08:35:38 +0700

    feat: add test connection function

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('fastify-cors');
 const jwt = require('fastify-jwt');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
+const sequelize = require('./config/db');
 const { typeDefs, resolvers } = require('./schema');
 
 const authPassport = require('./schema/auth/passport');
@@ -35,9 +36,19 @@ fastify.register(mercurius, {
   graphiql: true,
 });
 
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    throw new mercurius.ErrorWithProps(error);
+  }
+};
+
 const start = async () => {
   try {
     await fastify.listen(5001, '0.0.0.0');
+    await connect();
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
