69bc8d517cfdcb07ee2b5dd509605132436b4368
yozz <yozzdharma@gmail.com>
2022-03-17 11:03:52 +0700

    feat: add mpr item model

--- server/schema/mpr/model.js
+++ server/schema/mpr/model.js
@@ -140,6 +140,216 @@ const MPRMODULE = sequelize.define('module', {
   underscored: true,
 });
 
+const MPRITEM = sequelize.define('item', {
+  id: {
+    type: DataTypes.BIGINT(20).UNSIGNED,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  bomDescription: {
+    type: DataTypes.STRING,
+  },
+  bomSpecification: {
+    type: DataTypes.TEXT,
+  },
+  bomModel: {
+    type: DataTypes.STRING,
+  },
+  bomBrand: {
+    type: DataTypes.STRING,
+  },
+  bomQty: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomUnit: {
+    type: DataTypes.STRING,
+  },
+  bomQtyRqd: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomQtyBalance: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomQtyStock: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomEta: {
+    type: DataTypes.DATE,
+    get() {
+      if (this.getDataValue('bomEta') === '0000-00-00' || !this.getDataValue('bomEta')) return '';
+      return this.getDataValue('bomEta');
+    },
+  },
+  bomQtyRec: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomDateRec: {
+    type: DataTypes.DATE,
+    get() {
+      // Note: need to check po arrival date & add more conditions
+      if (this.getDataValue('bomDateRec') === '0000-00-00' || !this.getDataValue('bomDateRec')) return '';
+      return this.getDataValue('bomDateRec');
+    },
+  },
+  bomCurrSizeC: {
+    type: DataTypes.STRING,
+  },
+  bomCurrSizeV: {
+    type: DataTypes.STRING,
+    get() {
+      return parseFloat(this.getDataValue('bomCurrSizeV')) || 0;
+    },
+  },
+  bomCurrEaC: {
+    type: DataTypes.STRING,
+  },
+  bomCurrEaV: {
+    type: DataTypes.STRING,
+    get() {
+      return parseFloat(this.getDataValue('bomCurrEaV')) || 0;
+    },
+  },
+  bomUsdEa: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomUsdUnit: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomUsdTotal: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  bomSupplier: {
+    type: DataTypes.STRING,
+  },
+  bomPoDate: {
+    type: DataTypes.DATE,
+    get() {
+      if (this.getDataValue('bomPoDate') === '0000-00-00' || !this.getDataValue('bomPoDate')) return '';
+      return this.getDataValue('bomPoDate');
+    },
+  },
+  bomPoNo: {
+    type: DataTypes.STRING,
+  },
+  poZone: {
+    type: DataTypes.STRING,
+  },
+  poNo: {
+    type: DataTypes.STRING,
+  },
+  bomRemarks: {
+    type: DataTypes.TEXT,
+    get() {
+      const arr = ['<p> </p>', '<p><br></p>'];
+      return arr.includes(this.getDataValue('bomRemarks')) ? '' : this.getDataValue('bomRemarks');
+    },
+  },
+  bomEtaStatus: {
+    type: DataTypes.STRING,
+    get() {
+      // Note: need to check po arrival date & add more conditions
+      if (this.bomQty > 0 && this.bomQtyStock > 0 && this.bomQtyBalance >= 0) return 'ONTIME';
+      return '';
+    },
+  },
+  bomStatus: {
+    type: DataTypes.INTEGER,
+  },
+  whRemarks: {
+    type: DataTypes.TEXT,
+  },
+  prRemarks: {
+    type: DataTypes.TEXT,
+  },
+  rndRemarks: {
+    type: DataTypes.TEXT,
+  },
+  hvacRemarks: {
+    type: DataTypes.TEXT,
+  },
+  mechanicalRemarks: {
+    type: DataTypes.TEXT,
+  },
+  electronicRemarks: {
+    type: DataTypes.TEXT,
+  },
+  fabricationRemarks: {
+    type: DataTypes.TEXT,
+  },
+  sr: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  isMpr: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+  validasi: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  packing: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  cancel: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  poQty: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  poCurr: {
+    type: DataTypes.STRING,
+  },
+  poVal: {
+    type: DataTypes.STRING,
+  },
+  poRemarks: {
+    type: DataTypes.TEXT,
+  },
+  materialsProcessed: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  yetToPurchase: {
+    type: DataTypes.DOUBLE(18, 2),
+  },
+  idMpr: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  idWo: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  idModule: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  idHeader: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  idMaterial: {
+    type: DataTypes.BIGINT(20),
+  },
+  timestamp: {
+    type: DataTypes.DATE(6),
+    allowNull: false,
+  },
+  hold: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  priority: {
+    type: DataTypes.STRING,
+  },
+}, {
+  tableName: 'mpr_item',
+  underscored: true,
+});
+
 module.exports = {
-  MPR, MPRMODULE,
+  MPR, MPRMODULE, MPRITEM,
 };
