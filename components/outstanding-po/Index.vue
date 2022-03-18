c6f1599cdf9ca16ac7f478582500f317146f2cde
yozz <yozzdharma@gmail.com>
2022-03-18 14:24:39 +0700

    feat: add complete column

--- components/outstanding-po/Index.vue
+++ components/outstanding-po/Index.vue
@@ -189,6 +189,12 @@
                 {{ scope.row.arrivalStatus }}
               </template>
             </el-table-column>
+            <el-table-column
+              label="Complete"
+              prop="comp"
+              align="center"
+              width="70"
+            ></el-table-column>
             <el-table-column label="" min-width="50"></el-table-column>
           </el-table>
         </div>
