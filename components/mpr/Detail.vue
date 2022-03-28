218b084100953d6348e332d97e969c019147bff3
yozz <yozzdharma@gmail.com>
2022-03-28 11:41:36 +0700

    feat(components): mpr detail base page

--- components/mpr/Detail.vue
+++ components/mpr/Detail.vue
@@ -1,5 +1,112 @@
 <template>
-  <div>
-    Hey, {{ $route.params.id }}
+  <div class="flex flex-col space-y-8">
+    <IndexErrorHandler
+      v-if="errors.length"
+      :errors="errors"
+    />
+
+    <div
+      v-loading.fullscreen.lock="$apollo.loading"
+      class="flex flex-col divide-y divide-gray-400 divide-dashed"
+      element-loading-text="Loading..."
+      element-loading-spinner="el-icon-loading"
+    >
+      <el-breadcrumb separator="/" class="mb-4">
+        <el-breadcrumb-item :to="{ name: 'index' }">
+          <outline-home-icon class="heroicons w-15px h-15px" />
+        </el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ name: 'mpr'}">
+          MPR
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>
+          ITEMS
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+      <div class="flex space-x-4 items-center">
+        <div class="flex flex-col space-y-4 w-full my-4">
+          <div class="flex space-x-8 items-center">
+            <div class="font-bold text-xl">
+              Info
+            </div>
+            <div class="flex-1"></div>
+            <div>
+              <el-popover
+                placement="left"
+                trigger="hover"
+              >
+                <template #default>
+                  <table class="plain">
+                    <tbody>
+                      <tr>
+                        <td class="w-6 bg-[#e1bee7]"></td>
+                        <td class="pl-2">
+                          Validated
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#64b5f6]"></td>
+                        <td class="pl-2">
+                          Stock
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#ffccbc]"></td>
+                        <td class="pl-2">
+                          PO Issued
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#a5d6a7]"></td>
+                        <td class="pl-2">
+                          Coming
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#ffa726]"></td>
+                        <td class="pl-2">
+                          Draft PO / PR
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#b0bec5]"></td>
+                        <td class="pl-2">
+                          Hold
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#ef5350]"></td>
+                        <td class="pl-2">
+                          Cancel
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="bg-[#ffffff]"></td>
+                        <td class="pl-2">
+                          Open
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </template>
+                <outline-information-circle-icon
+                  slot="reference"
+                  class="heroicons w-4 h-4 text-blue-600"
+                />
+              </el-popover>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      errors: [],
+    };
+  },
+};
+</script>
