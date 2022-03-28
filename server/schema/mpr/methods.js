13501e42dfb52c76aded4820fbb597d5ee5a9511
yozz <yozzdharma@gmail.com>
2022-03-28 09:19:20 +0700

    feat(server): array for department 320

--- server/schema/mpr/methods.js
+++ server/schema/mpr/methods.js
@@ -81,6 +81,7 @@ const whereUser = async ({
   else if (department === 170) inSection = [171];
   else if (department === 210) inSection = [211, 212, 213];
   else if (department === 220) inSection = [221];
+  else if (department === 320) inSection = [321, 322, 323, 324];
 
   if ((group === 11 && section === 211) || (group === 11 && section === 212)) {
     where = {
