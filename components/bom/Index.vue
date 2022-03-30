495ffad9286a47e3f851a14aaac4a214892746d0
yozz <yozzdharma@gmail.com>
2022-03-30 13:18:25 +0700

    feat(components): divider for bom page

--- components/bom/Index.vue
+++ components/bom/Index.vue
@@ -5,88 +5,108 @@
       :errors="errors"
     />
 
-    <div class="flex space-x-4 items-center">
-      <div class="w-48">
-        <el-select
-          v-model="status"
-          placeholder="Select"
-          class="el-select__primary"
-          @change="handleChange"
-        >
-          <el-option
-            v-for="item in optionsWoStatus"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          >
-          </el-option>
-        </el-select>
-      </div>
-      <div class="flex-1"></div>
-      <div class="w-64">
-        <el-input
-          v-model="search"
-          placeholder="Search"
-          clearable
-        />
-      </div>
-    </div>
+    <div class="flex flex-col divide-y divide-gray-400 divide-dashed">
+      <el-breadcrumb separator="/" class="mb-4">
+        <el-breadcrumb-item :to="{ name: 'index' }">
+          <outline-home-icon class="heroicons w-15px h-15px" />
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>
+          BOM
+        </el-breadcrumb-item>
+      </el-breadcrumb>
 
-    <div>
-      <el-table
-        v-loading="$apollo.loading"
-        element-loading-text="Loading..."
-        element-loading-spinner="el-icon-loading"
-        :data="tableData"
-        size="small"
-        border
-      >
-        <el-table-column type="index" align="center" width="50"></el-table-column>
-        <el-table-column label="LT" width="200">
-          <template slot-scope="scope">
-            <div class="flex">
-              <nuxt-link
-                :to="{ name: 'bom-lt-id-status', params: { id: scope.row.id, status } }"
-                :title="scope.row.ltNo"
-                class="flex-1 truncate"
+      <div>
+        <div class="flex my-4 space-x-8 items-center">
+          <div class="w-48">
+            <el-select
+              v-model="status"
+              placeholder="Select"
+              class="el-select__primary"
+              @change="handleChange"
+            >
+              <el-option
+                v-for="item in optionsWoStatus"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
               >
-                {{ scope.row.ltNo }}
-                <span v-if="scope.row.stage">[STAGE-{{ scope.row.stage }}]</span>
-              </nuxt-link>
-              <a
-                :href="`/bom/lt/${scope.row.id}/${status}`"
-                title="Open in new tab"
-                target="_blank"
-              >
-                <outline-external-link-icon class="heroicons w-4 h-4" />
-              </a>
-            </div>
-          </template>
-        </el-table-column>
-        <el-table-column prop="customer.name" label="Customer" width="400"></el-table-column>
-        <el-table-column label="WO" min-width="50">
-          <template slot-scope="scope">
-            {{ scope.row.wos.length }}
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
-    <div>
-      <el-pagination
-        :current-page.sync="page"
-        :page-sizes="pageSizes"
-        :page-size="pageSize"
-        :total="search ? tableData.length : items.length"
-        :pager-count="pagerCount"
-        layout="slot, sizes, prev, pager, next"
-        class="flex justify-end"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      >
-        <template #default>
-          <span class="font-normal">Total {{ tableData.length }}/{{ items.length }}</span>
-        </template>
-      </el-pagination>
+              </el-option>
+            </el-select>
+          </div>
+          <div class="flex-1"></div>
+          <div class="w-64">
+            <el-input
+              v-model="search"
+              placeholder="Search"
+              clearable
+            />
+          </div>
+        </div>
+      </div>
+
+      <div v-if="tableData.length">
+        <div class="flex flex-col space-y-4 my-4">
+          <div class="font-bold text-xl">
+            Status
+          </div>
+          <div>
+            <el-table
+              v-loading="$apollo.loading"
+              element-loading-text="Loading..."
+              element-loading-spinner="el-icon-loading"
+              :data="tableData"
+              size="small"
+              border
+            >
+              <el-table-column type="index" align="center" width="50"></el-table-column>
+              <el-table-column label="LT" width="200">
+                <template slot-scope="scope">
+                  <div class="flex">
+                    <nuxt-link
+                      :to="{ name: 'bom-lt-id-status', params: { id: scope.row.id, status } }"
+                      :title="scope.row.ltNo"
+                      class="flex-1 truncate"
+                    >
+                      {{ scope.row.ltNo }}
+                      <span v-if="scope.row.stage">[STAGE-{{ scope.row.stage }}]</span>
+                    </nuxt-link>
+                    <a
+                      :href="`/bom/lt/${scope.row.id}/${status}`"
+                      title="Open in new tab"
+                      target="_blank"
+                    >
+                      <outline-external-link-icon class="heroicons w-4 h-4" />
+                    </a>
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column prop="customer.name" label="Customer" width="400"></el-table-column>
+              <el-table-column label="WO" min-width="50">
+                <template slot-scope="scope">
+                  {{ scope.row.wos.length }}
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+          <div>
+            <el-pagination
+              :current-page.sync="page"
+              :page-sizes="pageSizes"
+              :page-size="pageSize"
+              :total="search ? tableData.length : items.length"
+              :pager-count="pagerCount"
+              layout="slot, sizes, prev, pager, next"
+              class="flex justify-end mt-4"
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+            >
+              <template #default>
+                <span class="font-normal">Total {{ tableData.length }}/{{ items.length }}</span>
+              </template>
+            </el-pagination>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
