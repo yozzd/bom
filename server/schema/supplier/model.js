ac56248e567c2b0b5668dbbd46b7e81f860c2eb9
yozz <yozzdharma@gmail.com>
2022-04-07 08:22:03 +0700

    feat(server): create supplier model

---
+++ server/schema/supplier/model.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../config/db');
+
+const Supplier = sequelize.define('supplier', {
+  suplierID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  suplierNM: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ContactPerson: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  Address: {
+    type: DataTypes.TEXT,
+  },
+  Country: {
+    type: DataTypes.STRING,
+  },
+  PostCode: {
+    type: DataTypes.STRING,
+  },
+  MobilePerson: {
+    type: DataTypes.STRING,
+  },
+  OfficePhone: {
+    type: DataTypes.STRING,
+  },
+  OfficePhone1: {
+    type: DataTypes.STRING,
+  },
+  Email: {
+    type: DataTypes.STRING,
+  },
+  HomePage: {
+    type: DataTypes.STRING,
+  },
+  Remark: {
+    type: DataTypes.TEXT,
+  },
+}, {
+  tableName: 'suplier',
+  timestamp: false,
+});
+
+module.exports = { Supplier };
