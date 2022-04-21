0bc5b688553c857c14f367a29188fb48b11fb368
yozz <yozzdharma@gmail.com>
2022-04-21 11:30:24 +0700

    feat(components): add remark field for user 211

--- components/outstanding-po/Edit.vue
+++ components/outstanding-po/Edit.vue
@@ -283,6 +283,16 @@
               <IndexEditor v-model="form.poRemarks" />
             </client-only>
           </el-form-item>
+          <el-form-item
+            v-if="$auth.$state.user.section === 211"
+            label="Remarks"
+            prop="poRemarksBom"
+            class="col-span-2"
+          >
+            <client-only>
+              <IndexEditor v-model="form.poRemarksBom" />
+            </client-only>
+          </el-form-item>
         </div>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -342,6 +352,8 @@ export default {
         comp: '',
         hse: '',
         poCancel: 0,
+        poRemarks: '',
+        poRemarksBom: '',
       },
       rules: {
         poIssue: [{ required: true, message: 'This field is required' }],
