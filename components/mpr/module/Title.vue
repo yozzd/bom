2dd351fe6dda32a193c3b70715557cea85117672
yozz <yozzdharma@gmail.com>
2022-05-13 09:33:13 +0700

    feat(components): send emit object for add module title

--- components/mpr/module/Title.vue
+++ components/mpr/module/Title.vue
@@ -103,7 +103,7 @@ export default {
 
             cdata.getOneMPR.modules.push(addMprModule);
 
-            this.$emit('update', cdata.getOneMPR.modules);
+            this.$emit('update', { type: 'module', values: cdata.getOneMPR.modules });
 
             store.writeQuery({
               query: GetOneMPR,
